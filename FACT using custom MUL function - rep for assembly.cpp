b96042bb49116bb150e97e0fca30da6ef349bcfb
Lari <88565631+romugaboo@users.noreply.github.com>
2022-01-20 10:53:22 -0300

    Update and rename MUL function - rep for MUL in assembly.cpp to FACT using custom MUL function - rep for assembly.cpp

--- FACT using custom MUL function - rep for assembly.cpp
+++ FACT using custom MUL function - rep for assembly.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 int MUL (int a, int b);
+int FACT (int n);
 
 int main() {
-  int a = 7, b = 8;
+  int a = 3, b = 4, n = 6;
   cout << MUL (a, b) << "\n";
+  cout << FACT (n) << "\n";
 } 
 
 int MUL (int a, int b){
@@ -14,4 +16,11 @@ int MUL (int a, int b){
     c = c + a;
   }
   return c;
+}
+
+int FACT (int n){
+  if (n <= 1)
+  return 1;
+  else
+  return MUL (n, FACT(n - 1));
 }
